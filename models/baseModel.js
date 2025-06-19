94e2b069c735503c9a056e5ce8ba38caa6d351b7
Etherforge Studios <EtherforgeStudios@outlook.com>
2025-06-19 13:36:47 -0600

    actualizacion git

--- models/baseModel.js
+++
@@ -1,5 +0,0 @@
-const { Model,DataTypes } = require('sequelize');
-const sequelize = require('../config/db');  // Importamos la instancia de sequelize
-
-// Exportar sequelize y DataTypes
-module.exports = { sequelize,Model, DataTypes };
